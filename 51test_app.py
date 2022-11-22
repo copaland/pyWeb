ff4cc6d58b9717d30a21522021d6c1ef866c88b8
Nicky Kim <copa@seoultech.ac.kr>
2022-11-22 11:36:42 +0900

    Add files via upload

---
+++ 51test_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.write('# web test :sunglasses:')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
